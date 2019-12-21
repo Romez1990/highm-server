a2fe0d937ad3a041c27e2b907deb36b1b4126651
Romez <Romez1990@gmail.com>
2019-12-21 11:23:46 +0300

    feature(project): setup postgresql database

--- highm_server/settings.py
+++ highm_server/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'highm_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
