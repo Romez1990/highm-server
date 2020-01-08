566c0e272ce754c6207bd7f84a7ef0c1e4346eff
Romez <Romez1990@gmail.com>
2020-01-08 13:20:57 +0300

    feature(auth): create authentication

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path, include
 
+from highm_server.settings import DEBUG
+
 urlpatterns = [
+    path('auth/', include('rest_auth.urls')),
 ]
+
+if DEBUG:
+    urlpatterns += [
+        path('session-auth/', include('rest_framework.urls')),
+    ]
