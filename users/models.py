061b4895840fd893375ea4822db88b5330808586
Romez <Romez1990@gmail.com>
2020-01-12 17:30:31 +0300

    feature(users): create user profile and student resources

--- users/models.py
+++ users/models.py
@@ -1,11 +1,25 @@
 from django.db.models import (
     Model,
     CharField,
+    BooleanField,
     DateTimeField,
+    OneToOneField,
+    ForeignKey,
+    CASCADE,
+    PROTECT,
 )
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+
+class Profile(Model):
+    user = OneToOneField(User, on_delete=CASCADE, primary_key=True)
+    dark_mode = BooleanField(verbose_name='Dark mode', default=False)
+
+    def __str__(self):
+        return f'{self.user.username} {self.user.first_name} ' \
+               f'{self.user.last_name}'
 
 
 class Group(Model):
@@ -14,3 +28,12 @@ class Group(Model):
 
     def __str__(self):
         return self.name
+
+
+class Student(Model):
+    user = OneToOneField(User, on_delete=CASCADE, primary_key=True)
+    group = ForeignKey(Group, verbose_name='Group', related_name='students',
+                       on_delete=PROTECT)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 
 from .models import (
     Group,
@@ -15,3 +18,12 @@ class GroupViewSet(ModelViewSet):
     permission_classes = [IsTeacher]
     lookup_field = 'name'
     lookup_value_regex = '.+'
+
+    def destroy(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.students.count() or obj.unregistered_students.count():
+            raise ValidationError({
+                'number_of_students': ['Cannot delete non-empty group.'],
+            })
+        self.perform_destroy(obj)
+        return Response(status=HTTP_204_NO_CONTENT)
