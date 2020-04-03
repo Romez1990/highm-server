e10a1e4c5bce3cce9483175efc03134060e6aa91
Romez <Romez1990@gmail.com>
2020-04-03 08:43:37 +0300

    fix(users): fix user permissions

--- users/permissions.py
+++ users/permissions.py
@@ -20,14 +20,16 @@ class IsStudent(BasePermission):
         return request.user.is_authenticated and not request.user.is_staff
 
 
-def IsOwner(owner_field):
-    class Permission(BasePermission):
-        def has_object_permission(self, request, view, obj):
-            if request.user.is_superuser:
-                return True
-            if not hasattr(obj, owner_field):
-                raise ValueError(
-                    f'owner field {owner_field} not found')
-            return request.user.is_authenticated and request.user == getattr(obj, owner_field)
+class IsOwner:
+    def __new__(cls, owner_field: str):
+        class Permission(BasePermission):
+            def has_object_permission(self, request, view, obj):
+                if request.user.is_superuser:
+                    return True
+                if not hasattr(obj, owner_field):
+                    raise ValueError(
+                        f'owner field {owner_field} not found')
+                own_it = request.user == getattr(obj, owner_field)
+                return request.user.is_authenticated and own_it
 
-    return Permission
+        return Permission
