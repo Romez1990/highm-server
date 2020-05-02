b6305f9b3a410f6589da33e55712cd1c479dc84c
Romez <Romez1990@gmail.com>
2020-05-02 12:41:18 +0300

    feature(users): create profile

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 
 from highm_server.settings import DEBUG
+from .views import (
+    ProfileView,
+)
 
 urlpatterns = [
     path('auth/', include('rest_auth.urls')),
+    path('auth/profile/', ProfileView.as_view(), name='profile'),
 ]
 
 if DEBUG:
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    RetrieveUpdateAPIView,
+)
 
-# Create your views here.
+from .models import (
+    Profile,
+)
+from .serializers import (
+    ProfileSerializer,
+)
+from .permissions import (
+    IsAuthenticated,
+)
+
+
+class ProfileView(RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.profile
+
+    def get_queryset(self):
+        return Profile.objects.none()
