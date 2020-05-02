984af42f2c394b0ad3132a4f9f4e6357e1438365
Romez <Romez1990@gmail.com>
2020-05-02 12:41:18 +0300

    feature(users): create profile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.db.models import (
+    Model,
+    BooleanField,
+    OneToOneField,
+    CASCADE,
+)
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+
+class Profile(Model):
+    user = OneToOneField(User, on_delete=CASCADE, primary_key=True)
+    dark_mode = BooleanField(verbose_name='Dark mode', default=False)
+
+    def __str__(self):
+        return f'{self.user.username} {self.user.first_name} ' \
+               f'{self.user.last_name}'
