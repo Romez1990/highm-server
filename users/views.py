e980756255e4666c8ae161d3080d538e7da9697d
Romez <Romez1990@gmail.com>
2020-05-02 17:38:06 +0300

    feature(users): mind unregistered users

--- users/views.py
+++ users/views.py
@@ -1,12 +1,15 @@
+from typing import Type
 from django.http import Http404
 from rest_framework.generics import (
     GenericAPIView,
     RetrieveUpdateAPIView,
 )
+from rest_framework.serializers import Serializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import ValidationError, MethodNotAllowed
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
 from rest_auth.registration.views import VerifyEmailView as VerifyEmailViewBase
 
 from .models import (
@@ -14,11 +17,14 @@ from .models import (
     Teacher,
     Group,
     GROUP_ADMINS,
+    UnregisteredUser,
 )
 from .serializers import (
     ProfileSerializer,
     StudentSerializer,
     TeacherSerializer,
+    UnregisteredStudentSerializer,
+    UnregisteredTeacherCreateSerializer,
     GroupBasicSerializer,
     GroupSerializer,
     RegistrationCodeCheckSerializer,
@@ -39,21 +45,79 @@ class ProfileView(RetrieveUpdateAPIView):
 
 
 class RegistrableModelViewSet(ModelViewSet):
+    serializer_class_registered: Type[Serializer]
+    serializer_class_unregistered: Type[Serializer]
+
+    def get_queryset_registered(self):
+        raise NotImplementedError
+
+    def get_queryset_unregistered(self):
+        raise NotImplementedError
+
+    def is_registered(self) -> bool:
+        pk = self.kwargs['pk']
+        return pk[0] != 'c'
+
+    def get_queryset(self):
+        # django rest framework call this with rendering list without pk
+        if 'pk' in self.kwargs and not self.is_registered():
+            return self.get_queryset_unregistered()
+        return self.get_queryset_registered()
+
+    def get_serializer(self, *args, **kwargs):
+        # django rest framework call this with rendering list without pk
+        if 'pk' in self.kwargs and not self.is_registered():
+            return self.get_serializer_unregistered(*args, **kwargs)
+        return self.get_serializer_registered(*args, **kwargs)
+
+    def get_serializer_registered(self, *args, **kwargs):
+        kwargs['context'] = self.get_serializer_context()
+        return self.serializer_class_registered(*args, **kwargs)
+
+    def get_serializer_unregistered(self, *args, **kwargs):
+        kwargs['context'] = self.get_serializer_context()
+        return self.serializer_class_unregistered(*args, **kwargs)
+
+    def list(self, request: Request, **kwargs) -> Response:
+        registered_queryset = self.get_queryset_registered()
+        registered = self.get_serializer_registered(registered_queryset,
+                                                    many=True).data
+        unregistered_queryset = self.get_queryset_unregistered()
+        unregistered = self.get_serializer_unregistered(unregistered_queryset,
+                                                        many=True).data
+        return Response(registered + unregistered)
+
+    def create(self, request: Request, **kwargs) -> Response:
+        serializer = self.get_serializer_unregistered(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=HTTP_201_CREATED)
+
     def destroy(self, *args, **kwargs) -> Response:
-        raise ValidationError({
-            'non_field_errors': [
-                'Cannot delete a registered user.'
-            ]
-        })
+        if self.is_registered():
+            raise ValidationError({
+                'non_field_errors': [
+                    'Cannot delete a registered user.'
+                ]
+            })
+        return super().destroy(*args, **kwargs)
 
 
 class StudentViewSet(RegistrableModelViewSet):
     permission_classes = [IsTeacher]
-    serializer_class = StudentSerializer
+    serializer_class_registered = StudentSerializer
+    serializer_class_unregistered = UnregisteredStudentSerializer
 
-    def get_queryset(self):
+    def get_queryset_registered(self):
         user = self.request.user
         queryset = Student.objects
+        if user.is_superuser:
+            return queryset
+        return queryset.exclude(group__name=GROUP_ADMINS)
+
+    def get_queryset_unregistered(self):
+        user = self.request.user
+        queryset = UnregisteredUser.objects.filter(is_staff=False)
         if user.is_superuser:
             return queryset
         return queryset.exclude(group__name=GROUP_ADMINS)
@@ -61,8 +125,14 @@ class StudentViewSet(RegistrableModelViewSet):
 
 class TeacherViewSet(RegistrableModelViewSet):
     permission_classes = [IsAdmin]
-    serializer_class = TeacherSerializer
-    queryset = Teacher.objects
+    serializer_class_registered = TeacherSerializer
+    serializer_class_unregistered = UnregisteredTeacherCreateSerializer
+
+    def get_queryset_registered(self):
+        return Teacher.objects
+
+    def get_queryset_unregistered(self):
+        return UnregisteredUser.objects.filter(is_staff=True)
 
 
 class GroupViewSet(ModelViewSet):
@@ -89,7 +159,7 @@ class GroupViewSet(ModelViewSet):
 
     def destroy(self, *args, **kwargs) -> Response:
         group = self.get_object()
-        if group.students.exists():
+        if group.students.exists() or group.unregistered_students.exists():
             raise ValidationError({
                 'number_of_students': [
                     'Cannot delete non-empty group.',
