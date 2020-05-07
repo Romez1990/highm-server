da74ed8befbb860c3f595a1687c66b06114f7d1b
Romez <Romez1990@gmail.com>
2020-05-07 10:18:07 +0300

    fixup! feature(users): mind unregistered users

--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
 from rest_framework.authentication import TokenAuthentication
 from rest_auth.registration.views import (
     RegisterView as BaseRegisterView,
@@ -87,7 +88,7 @@ class RegistrableModelViewSet(ModelViewSet):
         serializer = self.serializer_class_unregistered(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=HTTP_201_CREATED)
 
     def destroy(self, *args, **kwargs) -> Response:
         if self.is_registered():
