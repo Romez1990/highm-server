e798b05c4c58d3d9a38e69576fc19f6cc74f1e08
Romez <Romez1990@gmail.com>
2020-05-18 21:20:33 +0300

    feature(LessonResult): store lesson results

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    ForeignKey,
+    CASCADE,
+    IntegerField,
+    FloatField,
+    BooleanField,
+)
+from django.contrib.postgres.fields import (
+    JSONField,
+)
 
-# Create your models here.
+from users.models import (
+    Student,
+)
+
+
+class LessonResult(Model):
+    student = ForeignKey(Student, CASCADE)
+    lesson_number = IntegerField()
+    grade = FloatField()
+
+    class Meta:
+        unique_together = ['student', 'lesson_number']
+
+
+class TaskResult(Model):
+    lesson_result = ForeignKey(LessonResult, CASCADE,
+                               related_name='task_results')
+    task_number = IntegerField()
+    right = BooleanField()
+    answer = JSONField()
+
+    class Meta:
+        unique_together = ['lesson_result', 'task_number']
--- lessons/views.py
+++ lessons/views.py
@@ -2,11 +2,16 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 from users.permissions import IsStudent
 from users.utils import get_n
 from .base import LessonBasicSerializer
 from .lessons import Lessons
+from .models import (
+    LessonResult,
+    TaskResult,
+)
 
 
 class LessonViewSet(ViewSet):
@@ -27,7 +32,16 @@ class LessonViewSet(ViewSet):
 
     @action(detail=True, methods=['post'])
     def check(self, request: Request, number: str) -> Response:
+        student = request.user.student
         number_int = int(number)
+
+        queryset = LessonResult.objects.filter(student=student,
+                                               lesson_number=number_int)
+        if queryset.exists():
+            raise ValidationError({
+                'detail': 'This lesson has been passed.'
+            })
+
         serializer = Lessons.get_lesson_results_serializer_or_404(
             number_int, data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -36,6 +50,17 @@ class LessonViewSet(ViewSet):
         lesson_results = Lessons.get_lesson_results_or_404(number_int, n,
                                                            results)
         check_results = lesson_results.check()
+
+        lesson_result = LessonResult.objects.create(student=student,
+                                                    lesson_number=number_int,
+                                                    grade=0)
+        task_answers = []
+        for number, answer in enumerate(results['answers'].values(), 1):
+            task_answer = TaskResult(lesson_result=lesson_result,
+                                     task_number=number, answer=answer)
+            task_answers.append(task_answer)
+        TaskResult.objects.bulk_create(task_answers)
+
         return Response({
             'results': check_results
         })
