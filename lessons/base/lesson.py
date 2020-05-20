9aa20b5abfeb8735c3f2b261175d5696a2ed10de
Romez <Romez1990@gmail.com>
2020-05-20 17:08:11 +0300

    feature(LessonBase): add passed field to LessonBasic

--- lessons/base/lesson.py
+++ lessons/base/lesson.py
@@ -2,24 +2,43 @@ from typing import List, Dict, Type
 from rest_framework.serializers import (
     Serializer,
     CharField,
+    BooleanField,
     ListField,
 )
 
+from users.models import Student
+from ..models import LessonResult
 from .task import TaskBase
 
 
-class LessonBasicSerializer(Serializer):
+class LessonBasicSerializerBase(Serializer):
     title = CharField(max_length=150)
 
 
-class LessonBaseSerializer(Serializer):
-    title = CharField(max_length=150)
+class LessonBasicSerializer(LessonBasicSerializerBase):
+    passed = BooleanField()
+
+
+class LessonBaseSerializer(LessonBasicSerializerBase):
     goals = ListField(child=CharField(max_length=300))
 
 
-class LessonBase:
-    serializer: Type[LessonBaseSerializer]
+class LessonBasicBaseBase:
     title: str
+
+
+class LessonBasicBase(LessonBasicBaseBase):
+    number: int
+    passed: bool
+
+    def __init__(self, student: Student) -> None:
+        queryset = LessonResult.objects.filter(student=student,
+                                               lesson_number=self.number)
+        self.passed = queryset.exists()
+
+
+class LessonBase(LessonBasicBaseBase):
+    serializer: Type[LessonBaseSerializer]
     goals: List[str] = []
     tasks: Dict[str, TaskBase]
 
