cc88592fcb23b1b73aec7a7e8ecfa3ca4019113e
Romez <Romez1990@gmail.com>
2020-05-26 16:46:32 +0300

    refactor(LessonViewSet): make using CustomLookupTypeRouter

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
 
+from highm_server.custom_lookup_type_router import CustomLookupTypeRouter
 from .views import (
     LessonViewSet,
     StatementListView,
     StatementDetailView,
 )
 
-lesson_router = SimpleRouter()
+lesson_router = CustomLookupTypeRouter()
 lesson_router.register('lesson', LessonViewSet, basename='lesson')
 
 urlpatterns = [
