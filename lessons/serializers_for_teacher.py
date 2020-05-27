cbf172fec7b304133964705d3774d24af6af3314
Romez <Romez1990@gmail.com>
2020-05-27 20:58:25 +0300

    feature(ResultViewSet): create results list

--- lessons/serializers_for_teacher.py
+++ lessons/serializers_for_teacher.py
@@ -1,8 +1,22 @@
 from rest_framework.serializers import (
+    ModelSerializer,
     Serializer,
     CharField,
+    IntegerField,
+)
+
+from .models import (
+    LessonResult,
 )
 
 
 class LessonBasicSerializer(Serializer):
     title = CharField(max_length=150)
+
+
+class LessonResultSerializer(ModelSerializer):
+    class Meta:
+        model = LessonResult
+        fields = ['id', 'student_id', 'n', 'grade', 'points', 'max_points']
+
+    max_points = IntegerField()
