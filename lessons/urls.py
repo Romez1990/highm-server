e311a36e75407dadd2bf46da20bd43b9286d9d5b
Romez <Romez1990@gmail.com>
2020-05-27 19:11:08 +0300

    feature(TeacherLessonViewSet): create teacher lesson view set

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 
 from highm_server.custom_lookup_type_router import CustomLookupTypeRouter
 from .views_for_student import (
     LessonViewSet,
 )
+from .views_for_teacher import (
+    TeacherLessonViewSet,
+)
 
 router = CustomLookupTypeRouter()
 router.register('lesson', LessonViewSet, basename='lesson')
 
+teacher_lesson_router = SimpleRouter()
+teacher_lesson_router.register('lesson', TeacherLessonViewSet,
+                               basename='teacher-lesson')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('teacher-panel/group/<str:group>/',
+         include(teacher_lesson_router.urls)),
 ]
