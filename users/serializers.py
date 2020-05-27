443595c1954e8192fb4a01eb2de896b78787594a
Romez <Romez1990@gmail.com>
2020-05-27 12:21:23 +0300

    feature(StudentBasicSerializer): create student basic serializer

--- users/serializers.py
+++ users/serializers.py
@@ -73,6 +73,15 @@ class ProfileSerializer(ModelSerializer):
         return 'student'
 
 
+class StudentBasicSerializer(ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['first_name', 'last_name']
+
+    first_name = CharField(source='user.first_name', read_only=True)
+    last_name = CharField(source='user.last_name', read_only=True)
+
+
 class StudentSerializer(ModelSerializer, UserSerializerMixin):
     class Meta:
         model = Student
