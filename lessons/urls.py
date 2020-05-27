bef3c6c698b411243352970e89c8ea72f8e0dd90
Romez <Romez1990@gmail.com>
2020-05-27 08:25:56 +0300

    feature(LessonViewSet): create lesson view set

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,4 +1,13 @@
 from django.urls import path, include
 
+from highm_server.custom_lookup_type_router import CustomLookupTypeRouter
+from .views import (
+    LessonViewSet,
+)
+
+router = CustomLookupTypeRouter()
+router.register('lesson', LessonViewSet, basename='lesson')
+
 urlpatterns = [
+    path('', include(router.urls)),
 ]
