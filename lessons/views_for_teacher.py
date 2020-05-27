6dbbf58bd789ed5379b1b54c1e4994562cfe1878
Romez <Romez1990@gmail.com>
2020-05-27 19:43:02 +0300

    feature(ResultViewSet): create result view set

--- lessons/views_for_teacher.py
+++ lessons/views_for_teacher.py
@@ -42,3 +42,37 @@ class TeacherLessonViewSet(ModelViewSet):
 
     def destroy(self, request: Request, *args, **kwargs) -> Response:
         raise MethodNotAllowed(request.method)
+
+
+class LessonResultViewSet(ModelViewSet):
+    permission_classes = [IsTeacher]
+
+    serializer_classes = {
+        'list': Serializer,
+        'create': Serializer,
+        'retrieve': Serializer,
+        'update': Serializer,
+        'partial_update': Serializer,
+        'destroy': Serializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializer_classes[self.action]
+
+    def list(self, request: Request, *args, **kwargs) -> Response:
+        raise MethodNotAllowed(request.method)
+
+    def create(self, request: Request, *args, **kwargs) -> Response:
+        raise MethodNotAllowed(request.method)
+
+    def retrieve(self, request: Request, *args, **kwargs) -> Response:
+        raise MethodNotAllowed(request.method)
+
+    def update(self, request: Request, *args, **kwargs) -> Response:
+        raise MethodNotAllowed(request.method)
+
+    def partial_update(self, request: Request, *args, **kwargs) -> Response:
+        raise MethodNotAllowed(request.method)
+
+    def destroy(self, request: Request, *args, **kwargs) -> Response:
+        raise MethodNotAllowed(request.method)
