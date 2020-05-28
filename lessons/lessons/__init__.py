58aa3dafecf5c5ca99974c514a6db852c1090722
Romez <Romez1990@gmail.com>
2020-05-28 07:39:03 +0300

    feature(lessons): remove all code

--- lessons/lessons/__init__.py
+++
@@ -1,1 +0,0 @@
-from .lessons import Lessons
--- lessons/utils/serializer.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.fields import ListField, Field
-
-
-class MatrixField:
-    def __new__(cls, *, child: Field) -> object:
-        return ListField(child=ListField(child=child))
