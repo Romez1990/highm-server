ddcb4167eac961e345ecac3181f64336b0e3fd47
Romez <Romez1990@gmail.com>
2020-06-02 08:21:16 +0300

    refactor(Lesson1/Task6): reuse code from task 3

--- lessons/lessons/l1/tasks/t6/answer.py
+++ lessons/lessons/l1/tasks/t6/answer.py
@@ -1,33 +1,15 @@
-from typing import (
-    Dict,
-    Any,
-)
-from rest_framework.serializers import (
-    IntegerField,
-)
-
-from lessons.base import AnswerBase
-from lessons.serializers_for_student import AnswerSerializerBase
+from ..t3.answer import Answer3Serializer, Answer3
 from .task import Task6
 from .steps.s1 import Step1
 
 
-class Answer6Serializer(AnswerSerializerBase):
-    determinant = IntegerField()
+class Answer6Serializer(Answer3Serializer):
+    pass
 
 
-class Answer6(AnswerBase):
+class Answer6(Answer3):
     _task_class = Task6
     _task: Task6
     _step_classes = [
         Step1,
     ]
-
-    def __init__(self, n: int, determinant: int) -> None:
-        super().__init__(n)
-        self.determinant = determinant
-
-    def to_json(self) -> Dict[str, Any]:
-        return {
-            'determinant': self.determinant,
-        }
