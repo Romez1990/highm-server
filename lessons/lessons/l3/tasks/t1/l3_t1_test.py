193c8bc1ef9159da1860cb7df41dc9d31fb995fe
Romez <Romez1990@gmail.com>
2020-06-03 11:53:54 +0300

    test(Lesson3/Task1): test points specifying with different tolerances

--- lessons/lessons/l3/tasks/t1/l3_t1_test.py
+++ lessons/lessons/l3/tasks/t1/l3_t1_test.py
@@ -8,6 +8,28 @@ def test_check():
     assert answer.points == answer.max_points()
 
 
+def test_check_tolerance():
+    from lessons.lessons.l3.tasks.t1.answer import Answer1
+    answers = {
+        'x': 0.97791,
+    }
+    answer = Answer1(n=1, **answers)
+    answer.check()
+    assert answer.points == answer.max_points() - 1
+    answers = {
+        'x': 0.9779,
+    }
+    answer = Answer1(n=1, **answers)
+    answer.check()
+    assert answer.points == answer.max_points() - 2
+    answers = {
+        'x': 0.977,
+    }
+    answer = Answer1(n=1, **answers)
+    answer.check()
+    assert answer.points == answer.max_points() - 3
+
+
 def test_check_fail():
     from lessons.lessons.l3.tasks.t1.answer import Answer1
     answers = {
